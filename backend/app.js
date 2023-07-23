62dfcec269e698cbc8c8609303a248ec3795caa9
ikenna <mrteks2022@gmail.com>
2023-07-23 06:19:09 -0700

    connected ai version

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://eshop-tutorial-pyri.vercel.app',],
+  origin: ['https://ecommerce-sureplug23-1xm1.vercel.app',],
   credentials: true
 }));
 
